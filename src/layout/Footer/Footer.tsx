4419b9f473e380d80907f684d717183304f6aef8
c1c2c3c4c5c <151120869+c1c2c3c4c5c@users.noreply.github.com>
2024-08-28 22:45:35 +0530

    Update Footer.tsx

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="h-[var(--footer-height)] w-full border-t py-3 text-center text-sm">
-      Niti's Task for The Alter Office
+     Task by Niti for The Alter Office
     </footer>
   );
 }
