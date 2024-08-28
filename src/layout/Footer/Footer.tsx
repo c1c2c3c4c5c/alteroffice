f9668d4943c3b8df7561993aad64ea8b1733e186
c1c2c3c4c5c <niti.cce21@sot.pdpu.ac.in>
2024-08-28 22:32:59 +0530

    files added

---
+++ src/layout/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="h-[var(--footer-height)] w-full border-t py-3 text-center text-sm">
+      Niti's Task for The Alter Office
+    </footer>
+  );
+}
